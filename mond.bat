2b6405b22ee6008edf531d3e6c9ec497ddea8a5c
reisir <reisirito@gmail.com>
2023-12-15 19:29:58 +0200

    use `setlocal` in batch file :3

--- mond.bat
+++ mond.bat
@@ -1,6 +1,5 @@
-@ECHO OFF
-
-SET PSScript=%~dp0MetersOnDemand.ps1
-
+@echo off
+setlocal
+set PSScript=%~dp0MetersOnDemand.ps1
 Powershell -ExecutionPolicy Bypass -File "%PSScript%" %*
-@REM Powershell -ExecutionPolicy Bypass -Command "& '%PSScript%' %*"
+endlocal
