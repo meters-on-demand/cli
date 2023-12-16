2299559563f691cee9ae8f76c88592003582e990
reisir <reisirito@gmail.com>
2023-12-17 00:59:13 +0200

    it's big enough for a minor version upgrade

--- MetersOnDemand.ps1
+++ MetersOnDemand.ps1
@@ -82,7 +82,7 @@ param (
 
 # Globals
 $Self = [PSCustomObject]@{ 
-    Version       = "v1.2.6"
+    Version       = "v1.3.0"
     Directory     = "#Mond"
     FileName      = "MetersOnDemand.ps1"
     BatFileName   = "mond.bat"
